ffece0d0e996927daaf7148472307ac85e7d4af6
Anna Becker and Charlie Carter <charles.h.carter@gmail.com>
2016-02-22 11:53:31 -0800

    Update app to connect to database.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,53 +1,36 @@
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 
 public class Task {
-  private static ArrayList<Task> instances = new ArrayList<Task>();
-
-  private String mDescription;
-  private boolean mCompleted;
-  private LocalDateTime mCreatedAt;
-  private int mId;
-
-
-  public Task(String description) {
-    mDescription = description;
-    mCreatedAt = LocalDateTime.now();
-    mCompleted = false;
-    instances.add(this);
-    mId = instances.size();
-  }
+  private int id;
+  private String description;
 
   public String getDescription() {
-    return mDescription;
-  }
-
-  public boolean isCompleted() {
-    return mCompleted;
-  }
-
-  public LocalDateTime getCreatedAt() {
-    return mCreatedAt;
-  }
-
-  public static ArrayList<Task> all() {
-    return instances;
+    return description;
   }
 
   public int getId() {
-    return mId;
+    return id;
   }
 
-  public static Task find(int id) {
-    try {
-      return instances.get(id - 1);
-    } catch (IndexOutOfBoundsException e) {
-      return null;
-    }
+  public Task(String description) {
+    this.description = description;
   }
 
-  public static void clear() {
-    instances.clear();
+public static List<Task> all() {
+  String sql = "SELECT id, description FROM Tasks";
+  try(Connection con = DB.sql2o.open()) {
+    return con.createQuery(sql).executeAndFetch(Task.class);
   }
+ }
 
+ @Override
+ public boolean equals(Object otherTask) {
+   if (!(otherTask instanceof Task)) {
+     return false;
+   } else {
+     Task newTask = (Task) otherTask;
+     return this.getDescription().equals(newTask.getDescription());
+   }
+ }
 }
