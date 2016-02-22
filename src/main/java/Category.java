1c3e642ce80e1debc1e854dea141a857c0c3e645
Anna Becker and Charlie Carter <anna.r.becker@gmail.com>
2016-02-22 11:01:10 -0800

    Add initial files.

---
+++ src/main/java/Category.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Category {
+
+  private static ArrayList<Category> instances = new ArrayList<Category>();
+  private String mName;
+  private ArrayList<Task> mTasks;
+  private int mId;
+
+  public Category(String name) {
+    mName = name;
+    mTasks = new ArrayList<Task>();
+    instances.add(this);
+    mId = instances.size();
+  }
+  public String getName() {
+    return mName;
+  }
+  public ArrayList<Task> getTasks() {
+    return mTasks;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static ArrayList<Category> all() {
+    return instances;
+  }
+  public static void clear() {
+    instances.clear();
+  }
+
+  public static Category find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException e) {
+      return null;
+    }
+  }
+
+  public void addTask (Task task) {
+    mTasks.add(task);
+  }
+}
